e109559a20ff35fa5b0467188f7aca868884a9b2
Konstantin1spb <kevlen02@mail.ru>
2024-02-13 18:11:27 +0300

    react calc v1.1

--- app/src/App.js
+++ app/src/App.js
@@ -2,36 +2,63 @@ import styles from './app.module.css';
 import { useState } from 'react';
 
 const App = () => {
-	const [operand1, setOperand1] = useState('operand1');
-	const [operator, setOperator] = useState('operator');
-	const [operand2, setOperand2] = useState('operand2');
-	const NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+	let [operand1, setOperand1] = useState('');
+	let [operator, setOperator] = useState('');
+	let [operand2, setOperand2] = useState('');
+	const NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 	return (
 		<main className={styles.main}>
 			<div className={styles.calc}>
 				<div className={styles.calcDisplay}>
 					<span>
-						{operand1} + {operator} + {operand2}
+						{operand1} {operator} {operand2}
 					</span>
 				</div>
 				<div className={styles.calcControls}>
 					<div className={styles.calcNums}>
-						<button>1</button>
-						<button>2</button>
-						<button>3</button>
-						<button>4</button>
-						<button>5</button>
-						<button>6</button>
-						<button>7</button>
-						<button>8</button>
-						<button>9</button>
-						<button>0</button>
+						{NUMS.map((num) => (
+							<button
+								onClick={
+									operator
+										? () => setOperand1(operand1 + num)
+										: () => setOperand2(operand2 + num)
+								}
+							>
+								{num}
+							</button>
+						))}
 					</div>
 					<div className={styles.calcOperators}>
-						<button>+</button>
-						<button>-</button>
-						<button>C</button>
-						<button>=</button>
+						<button onClick={() => setOperator('+')}>+</button>
+						<button onClick={() => setOperator('-')}>-</button>
+						<button
+							onClick={() => {
+								setOperand1('');
+								setOperator('');
+								setOperand2('');
+							}}
+						>
+							C
+						</button>
+						<button
+							onClick={() => {
+								switch (operator) {
+									case '+':
+										setOperand1(Number(operand1) + Number(operand2));
+										setOperand2('');
+										setOperator('');
+										break;
+									case '-':
+										setOperand1(Number(operand1) - Number(operand2));
+										setOperand2('');
+										setOperator('');
+										break;
+									default:
+								}
+							}}
+						>
+							=
+						</button>
 					</div>
 				</div>
 			</div>
